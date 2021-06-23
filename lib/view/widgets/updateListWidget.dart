2ef43c1fca0a290fe2f18cc312639a16c3e307f7
dnwakshe <[wakshe2@gmail.com]>
2021-06-24 00:47:30 +0530

    decoration done

--- lib/view/widgets/updateListWidget.dart
+++ lib/view/widgets/updateListWidget.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:stock_details/view/screens/mainScreens/article.dart';
 Widget updateList(){
@@ -12,7 +14,14 @@ Widget updateList(){
           Navigator.of(context).push(MaterialPageRoute(builder: (_)=>NewArticle(pageTitle: _data[index],)));
         },
               child: Card(
-              
+                elevation: 0,
+                shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                //   side: BorderSide(
+                //   color: Colors.grey.withOpacity(0.2),
+                //   width: 0.2,
+                // ),
+                ),
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: Column(                
@@ -25,26 +34,32 @@ Widget updateList(){
                       Expanded(
                         flex: 1,
                         child: Container( 
-                          padding: EdgeInsets.only(right: 5),
-                          height: 50,
-                          child: Image.asset("othersMainImage.jpg"),)
+                          padding: EdgeInsets.only(right: 2),
+                          height: 60,
+                         
+                                child: ClipRRect(                                  
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: Image.asset("assets/1903-ecommerce_channels_ft_image.jpg",fit:BoxFit.contain,height: 60 ,width: 60,)
+                                ),                      
+                            ),
                         
                         ),
                         Expanded(
                           flex: 2,                      
-                        child: RichText(                        
+                        child: RichText( 
+
                           text: TextSpan(
                             
                           text:_data[index]+" \n",
-                           style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Colors.purple),
+                           style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold,color: Colors.purple, height: 2),
                            
                             children: [
                               
-                              TextSpan(text:_data[index],style: TextStyle(fontSize: 18,color: Colors.black,)),
+                              TextSpan(text:_data[index],style: TextStyle(fontSize: 15,color: Colors.black,)),
                               
-                              TextSpan(text:" \n "+_metadata[index],style: TextStyle(fontSize: 18,color: Colors.grey)),
+                              TextSpan(text:" \n "+_metadata[index],style: TextStyle(fontSize: 10,color: Colors.grey)),
                               
-                              TextSpan(text:" - 7 min read",style: TextStyle(fontSize: 18,color: Colors.grey)),
+                              TextSpan(text:" - 7 min read",style: TextStyle(fontSize: 10,color: Colors.grey)),
                             ],                        
                           ),
 
